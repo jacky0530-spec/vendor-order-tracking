3c7bdb4a0c7bb37b310133b41376d749a67a2bcc
jacky0530-spec <jacky0530@gmail.com>
2026-08-27 18:04:38 +0800

    Add Supabase configuration

---
+++ config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: 'https://qibeuakrryqjaiwbcorg.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_Z3v4-hkvYZO7im_xGqJgFQ_RjaYZ5CU',
+  ADMIN_API_URL: 'https://qibeuakrryqjaiwbcorg.supabase.co/functions/v1/vendor-portal',
+  LINE_STATUS_URL: 'https://qibeuakrryqjaiwbcorg.supabase.co/functions/v1/line-status'
+};
